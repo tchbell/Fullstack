6f0dc0185da012d3195ce11ddcf088ed335bf577
unknown <tchbell@gmail.com>
2023-04-19 13:07:27 -0400

    refactored with async/await instead of Promises

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const dbConnect = require('./db/dbConnect');
-const User = require('./db/userModel');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const winston = require('winston');
+const dbConnect = require('./db/dbConnect');
+const User = require('./db/userModel');
+
 const auth = require('./auth');
 dbConnect();
 
@@ -32,97 +34,63 @@ app.get('/', (request, response, next) => {
 });
 
 // register endpoint
-app.post('/register', (request, response) => {
-  // hash the password
-  bcrypt
-    .hash(request.body.password, 10)
-    .then((hashedPassword) => {
-      // create a new user instance and collect the data
-      const user = new User({
-        email: request.body.email,
-        password: hashedPassword,
-      });
-
-      // save the new user
-      user
-        .save()
-        // return success if the new user is added to the database successfully
-        .then((result) => {
-          response.status(201).send({
-            message: 'User Created Successfully',
-            result,
-          });
-        })
-        // catch error if the new user wasn't added successfully to the database
-        .catch((error) => {
-          response.status(500).send({
-            message: 'Error creating user',
-            error,
-          });
-        });
-    })
-    // catch error if the password hash isn't successful
-    .catch((e) => {
-      response.status(500).send({
-        message: 'Password was not hashed successfully',
-        e,
-      });
+app.post('/register', async (request, response, next) => {
+  try {
+    const hashedPassword = await bcrypt.hash(request.body.password, 10);
+    const user = new User({
+      email: request.body.email,
+      password: hashedPassword,
     });
+    const result = await user.save();
+    response.status(201).send({
+      message: 'User Created Successfully',
+      result,
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 // login endpoint
-app.post('/login', (request, response) => {
-  // check if email exists
-  User.findOne({ email: request.body.email })
+app.post('/login', async (request, response, next) => {
+  try {
+    const user = await User.findOne({ email: request.body.email });
 
-    // if email exists
-    .then((user) => {
-      // compare the password entered and the hashed password found
-      bcrypt
-        .compare(request.body.password, user.password)
-
-        // if the passwords match
-        .then((passwordCheck) => {
-          // check if password matches
-          if (!passwordCheck) {
-            return response.status(400).send({
-              message: 'Passwords does not match',
-              error,
-            });
-          }
-
-          //   create JWT token
-          const token = jwt.sign(
-            {
-              userId: user._id,
-              userEmail: user.email,
-            },
-            'RANDOM-TOKEN',
-            { expiresIn: '24h' }
-          );
-
-          //   return success response
-          response.status(200).send({
-            message: 'Login Successful',
-            email: user.email,
-            token,
-          });
-        })
-        // catch error if password does not match
-        .catch((error) => {
-          response.status(400).send({
-            message: 'Passwords does not match',
-            error,
-          });
-        });
-    })
-    // catch error if email does not exist
-    .catch((e) => {
-      response.status(404).send({
+    if (!user) {
+      return response.status(404).send({
         message: 'Email not found',
-        e,
       });
+    }
+
+    const passwordCheck = await bcrypt.compare(
+      request.body.password,
+      user.password
+    );
+
+    if (!passwordCheck) {
+      return response.status(400).send({
+        message: 'Passwords does not match',
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user._id,
+        userEmail: user.email,
+      },
+      'RANDOM-TOKEN',
+      { expiresIn: '24h' }
+    );
+
+    response.status(200).send({
+      message: 'Login Successful',
+      email: user.email,
+
+      token,
     });
+  } catch (error) {
+    next(error);
+  }
 });
 
 // free endpoint
@@ -134,4 +102,20 @@ app.get('/free-endpoint', (request, response) => {
 app.get('/auth-endpoint', auth, (request, response) => {
   response.json({ message: 'You are authorized to access me' });
 });
+
+const logger = winston.createLogger({
+  level: 'error',
+  format: winston.format.json(),
+  transports: [new winston.transports.File({ filename: 'error.log' })],
+});
+
+// error handling middleware function
+app.use((err, req, res, next) => {
+  logger.error(err.stack); // log the error stack trace
+  res.status(500).send({
+    message: 'Something went wrong',
+    error: err.message, // send the error message to the client
+  });
+});
+
 module.exports = app;
